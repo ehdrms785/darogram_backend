3c50a8adc29e978fa680622b9481ab4064d03d76
Daro <ehdrms785@naver.com>
2020-07-08 09:13:30 +0900

    Setting-Second

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({
+  schema,
+  playground: true,
+  instrospection: false,
+});
+
+server.express.use(logger("dev"));
+
+server.start({ port: PORT }, () =>
+  console.log(`✅ Server running on http://localhost:${PORT}`)
+);
